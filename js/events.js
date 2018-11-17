5c5664e848a11a47028ce140818c256437af6edb
Nick Smith <smthnck@gmail.com>
2018-11-17 15:41:58 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,8 +11,13 @@ function frameIt(){
 }
 
 function pressIt(){
+<<<<<<< HEAD
   $('input').on('keydown',function(e){
     if(e.which===71){
+=======
+  $('form input').on('keydown',function(e){
+    if(e.key===83){
+>>>>>>> a0690ac275478be62d828f865e80903dec8c1476
       alert('you pressed g!');
     }
   })
